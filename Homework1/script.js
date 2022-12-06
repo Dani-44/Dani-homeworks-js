27f3744e1cb9518bc0634e369fab85f0238ca9ff
Dani-44 <119892748+Dani-44@users.noreply.github.com>
2022-12-06 20:17:11 +0100

    Add files via upload

---
+++ Homework1/script.js
@@ -0,0 +1,9 @@
+const phonePrice = 119.95
+const tax = 6
+const phoneQuantity = 30
+
+const phonePriceWithTax = phonePrice + tax
+console.log("Phone price with tax is", phonePriceWithTax)
+
+const phones = phonePriceWithTax * phoneQuantity
+console.log("The price of 30 phones is", phones)
